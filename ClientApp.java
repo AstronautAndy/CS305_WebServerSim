a95c80c15cc190b5fc94905bb749da498c1bab6f
Andrew Ortiz <Andrew Ortiz>
2017-02-10 17:12:54 -0600

    Last commit for now. I need to evaluate the design as a cohesive whole instead of looking at each individual part.

--- ClientApp.java
+++ ClientApp.java
@@ -12,15 +12,21 @@ public class ClientApp
      * The standard for inputs will be as follows:
      * args[0] = Propagation Delay
      * args[1] = Transmission Delay
-     * args[2] = Version of HTTP used 
-     * args[3] = URL to the web page we want to access
+     * args[2] = binary value representing whether the client will be non-persistent or persistent  
      */
     public static void main(String[] args) throws Exception
     {
         //create a new transport layer for client (hence false) (connect to server), and read in first line from keyboard
         int delay = 0;
+        boolean persistent = false; //Will be non-persistent by default
         DelayData.setPropagationDelay( Integer.parseInt(args[0]) ); 
         DelayData.setTransmissionDelay( Integer.parseInt(args[1]) );
+        if(args[2] == "1"){
+          persistent = true;  
+        } else{
+            persistent = false;
+        }
+        
         TransportLayer transportLayer = new TransportLayer(false);
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String line = reader.readLine();
